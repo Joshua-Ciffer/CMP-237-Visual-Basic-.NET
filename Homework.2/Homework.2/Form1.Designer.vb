3097639b22ee445599173f6cb606aba92c4e061d
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2019-02-07 10:02:43 -0500

    Completed Homework #2 program

--- Homework.2/Homework.2/Form1.Designer.vb
+++ Homework.2/Homework.2/Form1.Designer.vb
@@ -22,193 +22,205 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.weightTextBox = New System.Windows.Forms.TextBox()
-        Me.weightLabel = New System.Windows.Forms.Label()
-        Me.radioButtonGroupBox = New System.Windows.Forms.GroupBox()
-        Me.venusRadioButton = New System.Windows.Forms.RadioButton()
-        Me.marsRadioButton = New System.Windows.Forms.RadioButton()
-        Me.poundsWeightLabel = New System.Windows.Forms.Label()
-        Me.poundsLabel = New System.Windows.Forms.Label()
-        Me.kilogramsWeightLabel = New System.Windows.Forms.Label()
-        Me.kilogramsLabel = New System.Windows.Forms.Label()
-        Me.calculateButton = New System.Windows.Forms.Button()
-        Me.clearButton = New System.Windows.Forms.Button()
-        Me.yourWeightLabel = New System.Windows.Forms.Label()
-        Me.labelGroupBox = New System.Windows.Forms.GroupBox()
-        Me.selectPlanetLabel = New System.Windows.Forms.Label()
-        Me.radioButtonGroupBox.SuspendLayout()
-        Me.labelGroupBox.SuspendLayout()
-        Me.SuspendLayout()
-        '
-        'weightTextBox
-        '
-        Me.weightTextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.weightTextBox.Location = New System.Drawing.Point(766, 56)
-        Me.weightTextBox.Name = "weightTextBox"
-        Me.weightTextBox.Size = New System.Drawing.Size(385, 75)
-        Me.weightTextBox.TabIndex = 0
-        '
-        'weightLabel
-        '
-        Me.weightLabel.AutoSize = True
-        Me.weightLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.9!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.weightLabel.Location = New System.Drawing.Point(41, 56)
-        Me.weightLabel.Name = "weightLabel"
-        Me.weightLabel.Size = New System.Drawing.Size(664, 63)
-        Me.weightLabel.TabIndex = 1
-        Me.weightLabel.Text = "Your weight on Earth (lbs):"
-        '
-        'radioButtonGroupBox
-        '
-        Me.radioButtonGroupBox.Controls.Add(Me.selectPlanetLabel)
-        Me.radioButtonGroupBox.Controls.Add(Me.marsRadioButton)
-        Me.radioButtonGroupBox.Controls.Add(Me.venusRadioButton)
-        Me.radioButtonGroupBox.Location = New System.Drawing.Point(77, 189)
-        Me.radioButtonGroupBox.Name = "radioButtonGroupBox"
-        Me.radioButtonGroupBox.Size = New System.Drawing.Size(425, 264)
-        Me.radioButtonGroupBox.TabIndex = 2
-        Me.radioButtonGroupBox.TabStop = False
-        '
-        'venusRadioButton
-        '
-        Me.venusRadioButton.AutoSize = True
-        Me.venusRadioButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.9!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.venusRadioButton.Location = New System.Drawing.Point(100, 100)
-        Me.venusRadioButton.Name = "venusRadioButton"
-        Me.venusRadioButton.Size = New System.Drawing.Size(217, 67)
-        Me.venusRadioButton.TabIndex = 0
-        Me.venusRadioButton.TabStop = True
-        Me.venusRadioButton.Text = "Venus"
-        Me.venusRadioButton.UseVisualStyleBackColor = True
-        '
-        'marsRadioButton
-        '
-        Me.marsRadioButton.AutoSize = True
-        Me.marsRadioButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.9!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.marsRadioButton.Location = New System.Drawing.Point(100, 177)
-        Me.marsRadioButton.Name = "marsRadioButton"
-        Me.marsRadioButton.Size = New System.Drawing.Size(184, 67)
-        Me.marsRadioButton.TabIndex = 3
-        Me.marsRadioButton.TabStop = True
-        Me.marsRadioButton.Text = "Mars"
-        Me.marsRadioButton.UseVisualStyleBackColor = True
-        '
-        'poundsWeightLabel
-        '
-        Me.poundsWeightLabel.AutoSize = True
-        Me.poundsWeightLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.poundsWeightLabel.Location = New System.Drawing.Point(106, 100)
-        Me.poundsWeightLabel.Name = "poundsWeightLabel"
-        Me.poundsWeightLabel.Size = New System.Drawing.Size(129, 69)
-        Me.poundsWeightLabel.TabIndex = 3
-        Me.poundsWeightLabel.Text = "100"
-        '
-        'poundsLabel
-        '
-        Me.poundsLabel.AutoSize = True
-        Me.poundsLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.poundsLabel.Location = New System.Drawing.Point(276, 100)
-        Me.poundsLabel.Name = "poundsLabel"
-        Me.poundsLabel.Size = New System.Drawing.Size(107, 69)
-        Me.poundsLabel.TabIndex = 4
-        Me.poundsLabel.Text = "lbs"
-        '
-        'kilogramsWeightLabel
-        '
-        Me.kilogramsWeightLabel.AutoSize = True
-        Me.kilogramsWeightLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.kilogramsWeightLabel.Location = New System.Drawing.Point(106, 179)
-        Me.kilogramsWeightLabel.Name = "kilogramsWeightLabel"
-        Me.kilogramsWeightLabel.Size = New System.Drawing.Size(129, 69)
-        Me.kilogramsWeightLabel.TabIndex = 5
-        Me.kilogramsWeightLabel.Text = "100"
-        '
-        'kilogramsLabel
-        '
-        Me.kilogramsLabel.AutoSize = True
-        Me.kilogramsLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.kilogramsLabel.Location = New System.Drawing.Point(276, 179)
-        Me.kilogramsLabel.Name = "kilogramsLabel"
-        Me.kilogramsLabel.Size = New System.Drawing.Size(93, 69)
-        Me.kilogramsLabel.TabIndex = 6
-        Me.kilogramsLabel.Text = "kg"
-        '
-        'calculateButton
-        '
-        Me.calculateButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.calculateButton.Location = New System.Drawing.Point(142, 515)
-        Me.calculateButton.Name = "calculateButton"
-        Me.calculateButton.Size = New System.Drawing.Size(370, 133)
-        Me.calculateButton.TabIndex = 7
-        Me.calculateButton.Text = "Calculate"
-        Me.calculateButton.UseVisualStyleBackColor = True
-        '
-        'clearButton
-        '
-        Me.clearButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.clearButton.Location = New System.Drawing.Point(667, 515)
-        Me.clearButton.Name = "clearButton"
-        Me.clearButton.Size = New System.Drawing.Size(370, 133)
-        Me.clearButton.TabIndex = 8
-        Me.clearButton.Text = "Clear"
-        Me.clearButton.UseVisualStyleBackColor = True
-        '
-        'yourWeightLabel
-        '
-        Me.yourWeightLabel.AutoSize = True
-        Me.yourWeightLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.1!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.yourWeightLabel.Location = New System.Drawing.Point(6, 21)
-        Me.yourWeightLabel.Name = "yourWeightLabel"
-        Me.yourWeightLabel.Size = New System.Drawing.Size(497, 55)
-        Me.yourWeightLabel.TabIndex = 9
-        Me.yourWeightLabel.Text = "Your weight would be:"
-        '
-        'labelGroupBox
-        '
-        Me.labelGroupBox.Controls.Add(Me.poundsLabel)
-        Me.labelGroupBox.Controls.Add(Me.yourWeightLabel)
-        Me.labelGroupBox.Controls.Add(Me.kilogramsLabel)
-        Me.labelGroupBox.Controls.Add(Me.poundsWeightLabel)
-        Me.labelGroupBox.Controls.Add(Me.kilogramsWeightLabel)
-        Me.labelGroupBox.Location = New System.Drawing.Point(623, 189)
-        Me.labelGroupBox.Name = "labelGroupBox"
-        Me.labelGroupBox.Size = New System.Drawing.Size(515, 264)
-        Me.labelGroupBox.TabIndex = 10
-        Me.labelGroupBox.TabStop = False
-        '
-        'selectPlanetLabel
-        '
-        Me.selectPlanetLabel.AutoSize = True
-        Me.selectPlanetLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.9!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.selectPlanetLabel.Location = New System.Drawing.Point(6, 16)
-        Me.selectPlanetLabel.Name = "selectPlanetLabel"
-        Me.selectPlanetLabel.Size = New System.Drawing.Size(399, 63)
-        Me.selectPlanetLabel.TabIndex = 4
-        Me.selectPlanetLabel.Text = "Select a planet:"
-        '
-        'Form1
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 31.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1205, 720)
-        Me.Controls.Add(Me.labelGroupBox)
-        Me.Controls.Add(Me.clearButton)
-        Me.Controls.Add(Me.calculateButton)
-        Me.Controls.Add(Me.radioButtonGroupBox)
-        Me.Controls.Add(Me.weightLabel)
-        Me.Controls.Add(Me.weightTextBox)
-        Me.Name = "Form1"
-        Me.Text = "Planetary Weight Application"
-        Me.radioButtonGroupBox.ResumeLayout(False)
-        Me.radioButtonGroupBox.PerformLayout()
-        Me.labelGroupBox.ResumeLayout(False)
-        Me.labelGroupBox.PerformLayout()
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
+		Me.weightTextBox = New System.Windows.Forms.TextBox()
+		Me.weightLabel = New System.Windows.Forms.Label()
+		Me.radioButtonGroupBox = New System.Windows.Forms.GroupBox()
+		Me.selectPlanetLabel = New System.Windows.Forms.Label()
+		Me.marsRadioButton = New System.Windows.Forms.RadioButton()
+		Me.venusRadioButton = New System.Windows.Forms.RadioButton()
+		Me.poundsWeightLabel = New System.Windows.Forms.Label()
+		Me.poundsLabel = New System.Windows.Forms.Label()
+		Me.kilogramsWeightLabel = New System.Windows.Forms.Label()
+		Me.kilogramsLabel = New System.Windows.Forms.Label()
+		Me.calculateButton = New System.Windows.Forms.Button()
+		Me.clearButton = New System.Windows.Forms.Button()
+		Me.yourWeightLabel = New System.Windows.Forms.Label()
+		Me.labelGroupBox = New System.Windows.Forms.GroupBox()
+		Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+		Me.radioButtonGroupBox.SuspendLayout()
+		Me.labelGroupBox.SuspendLayout()
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SuspendLayout()
+		'
+		'weightTextBox
+		'
+		Me.weightTextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.weightTextBox.Location = New System.Drawing.Point(776, 56)
+		Me.weightTextBox.Name = "weightTextBox"
+		Me.weightTextBox.Size = New System.Drawing.Size(375, 75)
+		Me.weightTextBox.TabIndex = 0
+		'
+		'weightLabel
+		'
+		Me.weightLabel.AutoSize = True
+		Me.weightLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.9!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.weightLabel.Location = New System.Drawing.Point(41, 56)
+		Me.weightLabel.Name = "weightLabel"
+		Me.weightLabel.Size = New System.Drawing.Size(651, 61)
+		Me.weightLabel.TabIndex = 1
+		Me.weightLabel.Text = "Your weight on Earth (lbs):"
+		'
+		'radioButtonGroupBox
+		'
+		Me.radioButtonGroupBox.Controls.Add(Me.selectPlanetLabel)
+		Me.radioButtonGroupBox.Controls.Add(Me.marsRadioButton)
+		Me.radioButtonGroupBox.Controls.Add(Me.venusRadioButton)
+		Me.radioButtonGroupBox.Location = New System.Drawing.Point(77, 189)
+		Me.radioButtonGroupBox.Name = "radioButtonGroupBox"
+		Me.radioButtonGroupBox.Size = New System.Drawing.Size(455, 264)
+		Me.radioButtonGroupBox.TabIndex = 2
+		Me.radioButtonGroupBox.TabStop = False
+		'
+		'selectPlanetLabel
+		'
+		Me.selectPlanetLabel.AutoSize = True
+		Me.selectPlanetLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.9!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.selectPlanetLabel.Location = New System.Drawing.Point(6, 16)
+		Me.selectPlanetLabel.Name = "selectPlanetLabel"
+		Me.selectPlanetLabel.Size = New System.Drawing.Size(391, 61)
+		Me.selectPlanetLabel.TabIndex = 4
+		Me.selectPlanetLabel.Text = "Select a planet:"
+		'
+		'marsRadioButton
+		'
+		Me.marsRadioButton.AutoSize = True
+		Me.marsRadioButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.9!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.marsRadioButton.Location = New System.Drawing.Point(100, 177)
+		Me.marsRadioButton.Name = "marsRadioButton"
+		Me.marsRadioButton.Size = New System.Drawing.Size(182, 65)
+		Me.marsRadioButton.TabIndex = 3
+		Me.marsRadioButton.TabStop = True
+		Me.marsRadioButton.Text = "Mars"
+		Me.marsRadioButton.UseVisualStyleBackColor = True
+		'
+		'venusRadioButton
+		'
+		Me.venusRadioButton.AutoSize = True
+		Me.venusRadioButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.9!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.venusRadioButton.Location = New System.Drawing.Point(100, 100)
+		Me.venusRadioButton.Name = "venusRadioButton"
+		Me.venusRadioButton.Size = New System.Drawing.Size(213, 65)
+		Me.venusRadioButton.TabIndex = 0
+		Me.venusRadioButton.TabStop = True
+		Me.venusRadioButton.Text = "Venus"
+		Me.venusRadioButton.UseVisualStyleBackColor = True
+		'
+		'poundsWeightLabel
+		'
+		Me.poundsWeightLabel.AutoSize = True
+		Me.poundsWeightLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.poundsWeightLabel.Location = New System.Drawing.Point(106, 100)
+		Me.poundsWeightLabel.Name = "poundsWeightLabel"
+		Me.poundsWeightLabel.Size = New System.Drawing.Size(0, 69)
+		Me.poundsWeightLabel.TabIndex = 3
+		'
+		'poundsLabel
+		'
+		Me.poundsLabel.AutoSize = True
+		Me.poundsLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.poundsLabel.Location = New System.Drawing.Point(276, 100)
+		Me.poundsLabel.Name = "poundsLabel"
+		Me.poundsLabel.Size = New System.Drawing.Size(107, 69)
+		Me.poundsLabel.TabIndex = 4
+		Me.poundsLabel.Text = "lbs"
+		'
+		'kilogramsWeightLabel
+		'
+		Me.kilogramsWeightLabel.AutoSize = True
+		Me.kilogramsWeightLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.kilogramsWeightLabel.Location = New System.Drawing.Point(106, 179)
+		Me.kilogramsWeightLabel.Name = "kilogramsWeightLabel"
+		Me.kilogramsWeightLabel.Size = New System.Drawing.Size(0, 69)
+		Me.kilogramsWeightLabel.TabIndex = 5
+		'
+		'kilogramsLabel
+		'
+		Me.kilogramsLabel.AutoSize = True
+		Me.kilogramsLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.kilogramsLabel.Location = New System.Drawing.Point(276, 179)
+		Me.kilogramsLabel.Name = "kilogramsLabel"
+		Me.kilogramsLabel.Size = New System.Drawing.Size(93, 69)
+		Me.kilogramsLabel.TabIndex = 6
+		Me.kilogramsLabel.Text = "kg"
+		'
+		'calculateButton
+		'
+		Me.calculateButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.calculateButton.Location = New System.Drawing.Point(162, 960)
+		Me.calculateButton.Name = "calculateButton"
+		Me.calculateButton.Size = New System.Drawing.Size(370, 133)
+		Me.calculateButton.TabIndex = 7
+		Me.calculateButton.Text = "Calculate"
+		Me.calculateButton.UseVisualStyleBackColor = True
+		'
+		'clearButton
+		'
+		Me.clearButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.clearButton.Location = New System.Drawing.Point(672, 960)
+		Me.clearButton.Name = "clearButton"
+		Me.clearButton.Size = New System.Drawing.Size(370, 133)
+		Me.clearButton.TabIndex = 8
+		Me.clearButton.Text = "Clear"
+		Me.clearButton.UseVisualStyleBackColor = True
+		'
+		'yourWeightLabel
+		'
+		Me.yourWeightLabel.AutoSize = True
+		Me.yourWeightLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.1!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.yourWeightLabel.Location = New System.Drawing.Point(6, 21)
+		Me.yourWeightLabel.Name = "yourWeightLabel"
+		Me.yourWeightLabel.Size = New System.Drawing.Size(497, 55)
+		Me.yourWeightLabel.TabIndex = 9
+		Me.yourWeightLabel.Text = "Your weight would be:"
+		'
+		'labelGroupBox
+		'
+		Me.labelGroupBox.Controls.Add(Me.poundsLabel)
+		Me.labelGroupBox.Controls.Add(Me.yourWeightLabel)
+		Me.labelGroupBox.Controls.Add(Me.kilogramsLabel)
+		Me.labelGroupBox.Controls.Add(Me.poundsWeightLabel)
+		Me.labelGroupBox.Controls.Add(Me.kilogramsWeightLabel)
+		Me.labelGroupBox.Location = New System.Drawing.Point(623, 189)
+		Me.labelGroupBox.Name = "labelGroupBox"
+		Me.labelGroupBox.Size = New System.Drawing.Size(545, 264)
+		Me.labelGroupBox.TabIndex = 10
+		Me.labelGroupBox.TabStop = False
+		'
+		'PictureBox1
+		'
+		Me.PictureBox1.Image = Global.Homework._2.My.Resources.Resources.hqdefault_1_
+		Me.PictureBox1.Location = New System.Drawing.Point(109, 491)
+		Me.PictureBox1.Name = "PictureBox1"
+		Me.PictureBox1.Size = New System.Drawing.Size(985, 430)
+		Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox1.TabIndex = 11
+		Me.PictureBox1.TabStop = False
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 31.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(1205, 1189)
+		Me.Controls.Add(Me.PictureBox1)
+		Me.Controls.Add(Me.labelGroupBox)
+		Me.Controls.Add(Me.clearButton)
+		Me.Controls.Add(Me.calculateButton)
+		Me.Controls.Add(Me.radioButtonGroupBox)
+		Me.Controls.Add(Me.weightLabel)
+		Me.Controls.Add(Me.weightTextBox)
+		Me.Name = "Form1"
+		Me.Text = "Planetary Weight Application"
+		Me.radioButtonGroupBox.ResumeLayout(False)
+		Me.radioButtonGroupBox.PerformLayout()
+		Me.labelGroupBox.ResumeLayout(False)
+		Me.labelGroupBox.PerformLayout()
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
 
-    End Sub
+	End Sub
 
-    Friend WithEvents weightTextBox As TextBox
+	Friend WithEvents weightTextBox As TextBox
     Friend WithEvents weightLabel As Label
     Friend WithEvents radioButtonGroupBox As GroupBox
     Friend WithEvents venusRadioButton As RadioButton
@@ -222,4 +234,5 @@ Partial Class Form1
     Friend WithEvents yourWeightLabel As Label
     Friend WithEvents selectPlanetLabel As Label
     Friend WithEvents labelGroupBox As GroupBox
+	Friend WithEvents PictureBox1 As PictureBox
 End Class
--- Homework.2/Homework.2/Form1.vb
+++ Homework.2/Homework.2/Form1.vb
@@ -1,13 +1,39 @@
-﻿Public Class Form1
-    Private Sub venusRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles venusRadioButton.CheckedChanged
+﻿'Joshua Ciffer
+'02/07/2019
 
-    End Sub
+Public Class Form1
 
-    Private Sub weightTextBox_TextChanged(sender As Object, e As EventArgs) Handles weightTextBox.TextChanged
+	Private Sub calculateButton_Click(sender As Object, e As EventArgs) Handles calculateButton.Click
+		Dim earthWeight, venusWeight, marsWeight As Double
+		If IsNumeric(weightTextBox.Text) Then
+			If Convert.ToDouble(weightTextBox.Text) >= 0 Then
+				earthWeight = Convert.ToDouble(weightTextBox.Text)
+				venusWeight = Convert.ToDouble(earthWeight * 0.907)
+				marsWeight = Convert.ToDouble(earthWeight * 0.377)
+				If venusRadioButton.Checked = False And marsRadioButton.Checked = False Then
+					MsgBox("Please select a planet.", , "Select a Planet")
+				ElseIf venusRadioButton.Checked = True Then
+					poundsWeightLabel.Text = venusWeight
+					kilogramsWeightLabel.Text = venusWeight * 0.454
+				ElseIf marsRadioButton.Checked = True Then
+					poundsWeightLabel.Text = marsWeight
+					kilogramsWeightLabel.Text = marsWeight * 0.454
+				End If
+			Else
+				MsgBox("Please enter a positive value for your weight.", , "Negative Input")
+			End If
+		Else
+			MsgBox("Please enter a numeric value for your weight.", , "Non-Numeric Input")
+		End If
+	End Sub
 
-    End Sub
+	Private Sub clearButton_Click(sender As Object, e As EventArgs) Handles clearButton.Click
+		weightTextBox.Clear()
+		poundsWeightLabel.Text = ""
+		kilogramsWeightLabel.Text = ""
+		venusRadioButton.Checked = False
+		marsRadioButton.Checked = False
+		weightTextBox.Focus()
+	End Sub
 
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles poundsWeightLabel.Click
-
-    End Sub
 End Class
