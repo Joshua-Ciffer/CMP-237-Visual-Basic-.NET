2a04cd7511d51195ded1a7a7f1d5d5bf94e4448b
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2019-02-07 10:33:13 -0500

    Worked on Homework #1

--- Homework.1/Form1.Designer.vb
+++ Homework.1/Form1.Designer.vb
@@ -22,10 +22,102 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
-    End Sub
+		Me.bookTripButton = New System.Windows.Forms.Button()
+		Me.radioButtonGroupBox = New System.Windows.Forms.GroupBox()
+		Me.myrtleBeachRadioButton = New System.Windows.Forms.RadioButton()
+		Me.keyWestRadioButton = New System.Windows.Forms.RadioButton()
+		Me.veniceBeachRadioButton = New System.Windows.Forms.RadioButton()
+		Me.pictureBox = New System.Windows.Forms.PictureBox()
+		Me.radioButtonGroupBox.SuspendLayout()
+		CType(Me.pictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SuspendLayout()
+		'
+		'bookTripButton
+		'
+		Me.bookTripButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.9!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.bookTripButton.Location = New System.Drawing.Point(412, 713)
+		Me.bookTripButton.Name = "bookTripButton"
+		Me.bookTripButton.Size = New System.Drawing.Size(345, 123)
+		Me.bookTripButton.TabIndex = 0
+		Me.bookTripButton.Text = "Book Trip"
+		Me.bookTripButton.UseVisualStyleBackColor = True
+		'
+		'radioButtonGroupBox
+		'
+		Me.radioButtonGroupBox.Controls.Add(Me.veniceBeachRadioButton)
+		Me.radioButtonGroupBox.Controls.Add(Me.keyWestRadioButton)
+		Me.radioButtonGroupBox.Controls.Add(Me.myrtleBeachRadioButton)
+		Me.radioButtonGroupBox.Location = New System.Drawing.Point(2, 525)
+		Me.radioButtonGroupBox.Name = "radioButtonGroupBox"
+		Me.radioButtonGroupBox.Size = New System.Drawing.Size(1190, 155)
+		Me.radioButtonGroupBox.TabIndex = 2
+		Me.radioButtonGroupBox.TabStop = False
+		'
+		'myrtleBeachRadioButton
+		'
+		Me.myrtleBeachRadioButton.AutoSize = True
+		Me.myrtleBeachRadioButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.1!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.myrtleBeachRadioButton.Location = New System.Drawing.Point(42, 61)
+		Me.myrtleBeachRadioButton.Name = "myrtleBeachRadioButton"
+		Me.myrtleBeachRadioButton.Size = New System.Drawing.Size(341, 59)
+		Me.myrtleBeachRadioButton.TabIndex = 0
+		Me.myrtleBeachRadioButton.TabStop = True
+		Me.myrtleBeachRadioButton.Text = "Myrtle Beach"
+		Me.myrtleBeachRadioButton.UseVisualStyleBackColor = True
+		'
+		'keyWestRadioButton
+		'
+		Me.keyWestRadioButton.AutoSize = True
+		Me.keyWestRadioButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.1!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.keyWestRadioButton.Location = New System.Drawing.Point(440, 61)
+		Me.keyWestRadioButton.Name = "keyWestRadioButton"
+		Me.keyWestRadioButton.Size = New System.Drawing.Size(266, 59)
+		Me.keyWestRadioButton.TabIndex = 1
+		Me.keyWestRadioButton.TabStop = True
+		Me.keyWestRadioButton.Text = "Key West"
+		Me.keyWestRadioButton.UseVisualStyleBackColor = True
+		'
+		'veniceBeachRadioButton
+		'
+		Me.veniceBeachRadioButton.AutoSize = True
+		Me.veniceBeachRadioButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.1!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.veniceBeachRadioButton.Location = New System.Drawing.Point(767, 61)
+		Me.veniceBeachRadioButton.Name = "veniceBeachRadioButton"
+		Me.veniceBeachRadioButton.Size = New System.Drawing.Size(358, 59)
+		Me.veniceBeachRadioButton.TabIndex = 2
+		Me.veniceBeachRadioButton.TabStop = True
+		Me.veniceBeachRadioButton.Text = "Venice Beach"
+		Me.veniceBeachRadioButton.UseVisualStyleBackColor = True
+		'
+		'pictureBox
+		'
+		Me.pictureBox.Location = New System.Drawing.Point(29, 30)
+		Me.pictureBox.Name = "pictureBox"
+		Me.pictureBox.Size = New System.Drawing.Size(1120, 505)
+		Me.pictureBox.TabIndex = 3
+		Me.pictureBox.TabStop = False
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 31.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(1185, 885)
+		Me.Controls.Add(Me.pictureBox)
+		Me.Controls.Add(Me.radioButtonGroupBox)
+		Me.Controls.Add(Me.bookTripButton)
+		Me.Name = "Form1"
+		Me.Text = "Spring Break Travel Specials"
+		Me.radioButtonGroupBox.ResumeLayout(False)
+		Me.radioButtonGroupBox.PerformLayout()
+		CType(Me.pictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.ResumeLayout(False)
 
+	End Sub
+
+	Friend WithEvents bookTripButton As Button
+	Friend WithEvents radioButtonGroupBox As GroupBox
+	Friend WithEvents veniceBeachRadioButton As RadioButton
+	Friend WithEvents keyWestRadioButton As RadioButton
+	Friend WithEvents myrtleBeachRadioButton As RadioButton
+	Friend WithEvents pictureBox As PictureBox
 End Class
--- Homework.1/Form1.vb
+++ Homework.1/Form1.vb
@@ -1,3 +1,38 @@
-﻿Public Class Form1
+﻿'Joshua Ciffer
+'02/07/2019
+
+Public Class Form1
+
+	Private Sub bookTripButton_Click(sender As Object, e As EventArgs) Handles bookTripButton.Click
+		If veniceBeachRadioButton.Checked = False And keyWestRadioButton.Checked = False And myrtleBeachRadioButton.Checked = False Then
+			MsgBox("Please select a destination.", , "Error")
+		Else
+			If veniceBeachRadioButton.Checked = True Then
+				MsgBox("Enjoy your trip to Venice Beach.", , "Bon Voyage")
+				Close()
+			ElseIf keyWestRadioButton.Checked = True Then
+				MsgBox("Enjoy your trip to Key West.", , "Bon Voyage")
+				Close()
+			ElseIf myrtleBeachRadioButton.Checked = True Then
+				MsgBox("Enjoy your trip to Myrtle Beach.", , "Bon Voyage")
+				Close()
+			End If
+		End If
+	End Sub
+
+	Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles pictureBox.Click
+		If veniceBeachRadioButton.Checked = True Then
+			pictureBox.Image = Homework._1.Resources.veniceBeach.jpg
+			pictureBox.Visible = True
+		ElseIf keyWestRadioButton.Checked = True Then
+			pictureBox.Image = Image.FromFile(Properties.Resources.keyWest.jpg)
+			pictureBox.Visible = True
+		ElseIf myrtleBeachRadioButton.Checked = True Then
+			pictureBox.Image = Image.FromFile("Homework.1\Resources\myrtleBeach.jpg")
+			pictureBox.Visible = True
+		Else
+			pictureBox.Visible = False
+		End If
+	End Sub
 
 End Class
